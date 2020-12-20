fb18c61d163f0102f814295961059bc3a94e1b34
poncharelocarachata <71048648+poncharelocarachata@users.noreply.github.com>
2020-12-19 19:57:44 -0600

    Create lista de reyes.lua

---
+++ lista de reyes.lua
@@ -0,0 +1,47 @@
+
+--//====================================================\\--
+--||			   LISTA DE REYES MAGOS :D
+--\\====================================================//--
+
+
+print("LISTA DE REYES MAGOOOOS")
+
+-- MyTable --
+local Cosas = {PC tremenda, dineros}
+
+-- Variables --
+local PC tremenda ="https://www.ebay.com/itm/Lenovo-IdeaPad-S145-15API-15-6-Laptop-AMD-Ryzen-3-8GB-RAM-256GB-SSD-81UT00EAUS/143772748893?_trkparms=ispr%3D1&hash=item2179860c5d:g:0NgAAOSw9Vxfe4z2&amdata=enc%3AAQAFAAACcBaobrjLl8XobRIiIML1V4Imu%252Fn%252BzU5L90Z278x5ickk7d4nremBkvNKtcC0ZwqXDIE3J%252BbzhfHjDFYJHrqpqwu%252BaplCfp59RyP3laNFJNFQEd89C%252BV7LmpA56JoY7B2YXmyc6yjXzTEAKj%252BXwvaL3OkrWj7Q3TuASmfdLyA9q0EbF%252B%252F7GYjITXN2tbs6ChnFGJvhPUlQHbMFUQK91DujLscwh0ciynZEPH%252BQMNIy9J4kPta0F5VkoISYHnGhybSCMJBDQ9BomFiqzKJzAZmLd7gLSx3DJ60fsrpwDvDhEIzqvNFDQLrOpPKKXZkoUTm53SWeXPvt5pi9JUIaL7lFD4Bnl0gR7NfTq2tDO6l8lGjVgFGlaWelF5nD1P3kYTbXQ9vTeWrgoIUN%252BvdfcnPQCRHtffHQsg6ykVyV2Edd47Oyfv4VpoLfqVj4jt5oB3KF60NGXkjCF9lorMX3zUW95hu%252BA4GhBQ1aM4CWeiRxVzB1nEQqPGboNCR9EoJhSWXAkj329lMP7HxiHkqkxCajjFRNCPnQFOFxL3JrHOQoC64DUEaZz%252BouWMNcUjLbJpo4FZh9T4RwBaY4OvWrWbCNUs253L5nJs54YoT5Qr16Tc3DOxaot5IRWHfyJ7XcQKq1xpROu6BESo49rMLSns8VcZ98cWHb4jkvuakfINvwIh1%252B70Xx8GDB7dQr0%252F140zd6%252FG2p3zcBEkivznroZaCN5e5FtQTJX%252BHjRnGhW6c4mjtJvmrVwVne2MVtNxmznFTKTRlXk9gToZr7A3qyZ%252FlCbbStPBhsskCE9J5R5o7UaVYu4tBl5QZjcPuvDR4W0urdg%253D%253D%7Ccksum%3A1437727488937be47b52e54f46029fc402642c977850%7Campid%3APL_CLK%7Cclp%3A2334524" 
+local dineros = "dineros para acomularlos con los que me den mis papás, mis tíos,y abuelos en mi cumpleaños para poder comprarme la pc de la primera opción con el total de dinero :)"
+
+-- Loops and If --
+if Comportamiento de paco == TREMENDA MENTE BUENOOO :D then		
+     print("le dan una:"..(Cosas[1])
+     PacoFeliz = true
+elseif Comportamiento de paco == Bueno  then
+     print("le dan una:"..(Cosas[2])
+     PacoAgradecido:( = true
+else
+     print("paco triste")
+     PacoFeliz = false
+end
+
+-- Grateful --
+for i, v in pairs(Cosas) do
+   if v == (Cosas[1]) then
+   print("MUCHISIMAS GRACIAAAAAAAS")
+elseif (Cosas[2]) then
+print("gracias")
+end
+
+-- Sad ;( --
+coroutine.resume(coroutine.create(function()
+while PacoFeliz == false do
+print(" Paco Llora ")
+end
+end))
+ 
+-- End --
+print("aqui acaba mi hermosa lista y muchísimas gracias reyes magos si me traen algo de la lista :)"
+
+
+local Paco Prefiere = (Cosas[1]);
